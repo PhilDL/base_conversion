0e6b249ec4526c51be9d26646bcdd841fb2ae08f
Philippe L'ATTENTION <philippe.lattention@hotmail.com>
2016-04-06 08:23:18 +0400

    Cleanup index.php

--- index.php
+++ index.php
@@ -4,10 +4,22 @@ require 'vendor/autoload.php';
 
 $conversion = new Tools\Conversion();
 
-echo $conversion->dec_to_bin(77868426984269842);
+echo $conversion->dec_to_bin(99);
 
-echo "\r\n";
-echo $conversion->dec_to_hex(45);
-echo "\r\n";
+echo PHP_EOL;
 
-echo base_convert("70C558", 16, 2);
+echo $conversion->dec_to_hex(99);
+
+echo PHP_EOL;
+
+echo $conversion->hex_to_dec("A2DE");
+
+echo PHP_EOL;
+
+echo $conversion->bin_to_dec("1010101");
+
+echo PHP_EOL;
+
+echo $conversion->hex_to_bin("70C558");
+
+echo PHP_EOL;
